36dcaadd17c0b3704fb82abe46ad2e6940cbb5ca
Kirill Opekishev <grumpykir@gmail.com>
2019-02-11 16:58:34 +0300

    Initial Commit

---
+++ GKViper/GKViper.h
@@ -0,0 +1,19 @@
+//
+//  GKViper.h
+//  GKViper
+//
+//  Created by  Кирилл on 2/11/19.
+//  Copyright © 2019 AppCraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GKViper.
+FOUNDATION_EXPORT double GKViperVersionNumber;
+
+//! Project version string for GKViper.
+FOUNDATION_EXPORT const unsigned char GKViperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GKViper/PublicHeader.h>
+
+
